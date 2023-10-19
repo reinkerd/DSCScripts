229bbe897e22aa520b01b36f04b485f67ad44f9d
David Reinker <david.reinker@southsound911.org>
2023-10-19 14:09:05 -0700

    Splitting into mof/deploy

---
+++ ftp-deploy.ps1
@@ -0,0 +1,16 @@
+
+param ([string]$Server)
+
+# Hard-coded source location for modules and other files to copy to target server
+$source = "\\itdev46.lesa.net\temp"
+
+copy-item -path "c:\program files\windowspowershell\modules\ss911" -filter "*.*" -destination "$Source\Modules" -force -Recurse
+
+# Copy modules to target
+copy-item -path "$source\Modules" -filter "*.*" -Destination "\\$Server\c$\Program Files\WindowsPowerShell" -force -Recurse
+    
+#Create cim session to pass to start-dscconfiguration
+$cim=New-CimSession -computername $Server
+
+Start-DscConfiguration -CimSession $cim -path ".\FTPServers\" -verbose -force -wait
+
